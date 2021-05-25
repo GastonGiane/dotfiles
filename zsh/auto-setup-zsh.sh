c333a05f0a01555eb2303ea3655b0fb72d1ccd6b
GastonGiane <gaston.gianello@gmail.com>
2021-05-24 23:06:50 -0300

    one script to set the zsh with plugins

---
+++ zsh/auto-setup-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+cd ~/.config/zsh/
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/denysdovhan/spaceship-prompt.git --depth=1
